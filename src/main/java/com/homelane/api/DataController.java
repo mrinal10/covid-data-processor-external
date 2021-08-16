880ddaf042ab14b4d8cc61045899a084004ef1a8
Mrinal <mail.mrinal10@gmail.com>
2021-08-16 11:50:33 +0530

    commit

--- src/main/java/com/homelane/api/DataController.java
+++ src/main/java/com/homelane/api/DataController.java
@@ -4,12 +4,9 @@ import com.homelane.model.DateWiseData;
 import com.homelane.model.StateDateWiseData;
 import com.homelane.model.StateWiseData;
 import com.homelane.service.DataService;
-import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@Api("Related data on Covid")
-@RequestMapping("/v1/external-data")
 public class DataController {
 
     private final DataService dataService;
@@ -19,22 +16,18 @@ public class DataController {
     }
 
     @GetMapping("/get_date_info/{date}")
-    public DateWiseData getDateSpecificdata(
-            @PathVariable("date") String date) {
+    public DateWiseData getDateSpecificdata(@PathVariable("date") String date) {
         return dataService.searchDateSpecificData(date);
     }
 
-
     @GetMapping("/get_state_info/{state}")
-    public StateWiseData getStateSpecificdata(
-            @PathVariable("state") String state) {
+    public StateWiseData getStateSpecificdata(@PathVariable("state") String state) {
         return dataService.searchStateSpecificData(state);
     }
 
-
     @GetMapping("/pinpoint_state/{state}/on/{date}")
-    public StateDateWiseData getStateSpecificdata(
-            @PathVariable("state") String state, @PathVariable("date") String date) {
+    public StateDateWiseData getStateSpecificdata(@PathVariable("state") String state,
+                                                  @PathVariable("date") String date) {
         return dataService.searchDateAndStateSpecificData(state, date);
     }
 
